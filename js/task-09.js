c31422972af593a388f3d0decc28ab7f55aa4f36
Morozzzuk <115870040+Morozzzuk@users.noreply.github.com>
2023-03-03 22:32:49 +0200

    answer task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+//* 1. Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+//* 2. Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+const bodyEl = document.querySelector('body');
+// console.log(bodyEl);
+const changeColorEl = document.querySelector('.change-color');
+// console.log(changeColorEl);
+const colorEl = document.querySelector('.color');
+// console.log(colorEl);
+bodyEl.addEventListener('click', changeBodyColorRandom);
+
+function changeBodyColorRandom() {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = `${getRandomHexColor()}`;
+}
